a54cef20d5390a075dd4919af4dfda25b6f49e4d
Sergey Psekha <pss@ciklum.com>
2013-01-03 12:50:52 +0200

    MP-0001: added task

---
+++ Sources/MSBulidMongo/Tasks/DeployDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Build.Framework;
+
+namespace MSBulidMongo.Tasks
+{
+    public class DeployDatabase : ITask
+    {
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+
+        public bool Execute()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
