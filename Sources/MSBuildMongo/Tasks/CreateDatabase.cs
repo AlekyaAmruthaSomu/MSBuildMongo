5ba652f070e34da1ae9e7f85bb7a2cad886089c7
Sergey Psekha <pss@ciklum.com>
2013-01-04 15:01:59 +0200

    MP-0001: added CreateDatabase task;

---
+++ Sources/MSBuildMongo/Tasks/CreateDatabase.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using MongoDB.Driver;
+
+namespace MSBuildMongo.Tasks
+{
+    public class CreateDatabase : Task
+    {
+        [Required]
+        public string ConnectionString { get; set; }
+
+        [Required]
+        public string DatabaseName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Admin { get; set; }
+
+        public override bool Execute()
+        {
+            this.Log.LogMessage("Creating database: " + this.DatabaseName);
+            try
+            {
+                Debugger.Launch();
+                this.EnsureAccessRights();
+                this.CreateDb();
+            }
+            catch (Exception ex)
+            {
+                this.Log.LogErrorFromException(ex);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void CreateDb()
+        {
+            var url = new MongoUrl(this.ConnectionString);
+            MongoClientSettings settings = MongoClientSettings.FromUrl(url);
+
+            MongoUser user = null;
+            MongoCredentials credentials = this.GetUserCredentials(settings);
+            
+            if (credentials != null)
+            {
+                user = new MongoUser(credentials, !credentials.Admin);
+            }
+
+            settings.DefaultCredentials = null;
+            
+            MongoCredentials adminCredentials = GetAdminCredentials();
+            settings.CredentialsStore.AddCredentials("admin", adminCredentials);
+            var client = new MongoClient(settings);
+            MongoServer server = client.GetServer();
+            MongoDatabase db = server.GetDatabase(this.DatabaseName);
+
+            if (user == null)
+            {
+                return;
+            }
+
+            MongoUser dbUser = db.FindUser(user.Username);
+            if (user.Equals(dbUser))
+            {
+                return;
+            }
+            
+            db.AddUser(user);
+        }
+
+        private MongoCredentials GetUserCredentials(MongoClientSettings settings)
+        {
+            return string.IsNullOrEmpty(this.UserName) ? settings.DefaultCredentials : new MongoCredentials(this.UserName, this.Password, this.Admin);
+        }
+
+        private void EnsureAccessRights()
+        {
+            var url = new MongoUrl(this.ConnectionString);
+            MongoClientSettings settings = MongoClientSettings.FromUrl(url);
+            settings.DefaultCredentials = null;
+
+            MongoCredentials adminCredentials = GetAdminCredentials();
+            var client = new MongoClient(settings);
+            MongoServer server = client.GetServer();
+            MongoDatabase db = server.GetDatabase("admin");
+
+            MongoUser user = db.FindUser("root");
+            if (IsUserAdmin(user))
+            {
+                return;
+            }
+
+            user = new MongoUser(adminCredentials, false);
+            db.AddUser(user);
+        }
+
+        private static bool IsUserAdmin(MongoUser user)
+        {
+            return user != null && !user.IsReadOnly;
+        }
+
+        private static MongoCredentials GetAdminCredentials()
+        {
+            var adminCredentials = new MongoCredentials("root", "secretPassword", true);
+            return adminCredentials;
+        }
+    }
+}
--- Sources/MSBuildMongo/Tasks/DeployDatabase.cs
+++ Sources/MSBuildMongo/Tasks/DeployDatabase.cs
@@ -1,16 +1,58 @@
 ﻿using System;
+using System.Diagnostics;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MSBuildMongo.Tasks
 {
     public class DeployDatabase : Task
     {
+        [Required]
+        public string ConnectionString { get; set; }
+
+        [Required]
+        public string Database { get; set; }
+
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
         public override bool Execute()
         {
             try
             {
                 Log.LogMessage("Deploying database");
-                throw new NotImplementedException();
+                var settings = new MongoClientSettings();
+                
+                var client = new MongoClient(ConnectionString);
+                MongoServer server = client.GetServer();
+                server.Connect();
+                server.Disconnect();
+                Debugger.Launch();
+                var credentials = new MongoCredentials("admin", "qwerty", true);
+                var defaultCredentials = client.Settings.DefaultCredentials;
+                var database = server.GetDatabase(Database);
+                if (!string.IsNullOrWhiteSpace(defaultCredentials.Username))
+                {
+                    var user = database.FindUser(defaultCredentials.Username);
+                    if (user == null)
+                    {
+                        var c = new MongoCredentials(defaultCredentials.Username, defaultCredentials.Password);
+                        user = new MongoUser(c, false);
+                        database.AddUser(user);
+                    }
+                }
+
+                var collection = database.GetCollection("Users");
+                var document = new BsonDocument
+                    {
+                        {
+                            "name", BsonValue.Create("sergey")
+                        }
+                    };
+                collection.Save(document);
             }
             catch (Exception ex)
             {
